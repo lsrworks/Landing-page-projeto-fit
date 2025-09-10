32cd94d386037b8dc9686053c1601e9e96d23e6e
lsrworks <projetosonmkt@gmail.com>
2025-09-10 17:14:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var litespeed_docref=sessionStorage.getItem("litespeed_docref");litespeed_docref&&(Object.defineProperty(document,"referrer",{get:function(){return litespeed_docref}}),sessionStorage.removeItem("litespeed_docref"));
+if(typeof window.typebotWpUser==="undefined"){window.typebotWpUser={"WP ID":"0","WP Username":"","WP Email":"","WP First name":"","WP Last name":""}}
+
+
